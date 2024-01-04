5a022fc1443b10d7d534621d2c0c73b04d3c5e6b
Shane Mindreau <smindre1@binghamton.edu>
2024-01-04 14:33:02 -0500

    I finished making a typeDefs.js and index.js file in the directory server/schemas/ and will now be working on the resolvers.js file.

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,35 @@
+const typeDefs = `
+    type Book {
+        _id: ID
+        authors: [String]
+        description: String
+        bookId: String!
+        image: String
+        link: String
+        title: String!
+    }
+
+    type User {
+        _id: ID
+        username: String!
+        email: String!
+        password: String!
+        savedBooks: [Book]
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+      }
+
+    type Query {
+        me: User
+    }
+
+    type Mutation {
+        addProfile(username: String!, email: String!, password: String!): Auth
+        login(email: String!, password: String!): Auth
+        saveBook(authors: [String], description: String, title: String!, bookId: String!, image: String, link: String): User
+        removeBook(bookId: String!): User
+      }
+`;
