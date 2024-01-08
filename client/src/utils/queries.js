57e0dce50b4f6f194bf488a16d8252286dd23098
Shane Mindreau <smindre1@binghamton.edu>
2024-01-07 23:57:23 -0500

    I added content to the queries.js file to the client/src/utils/ directory. I updated the vite.config.js in the client directory. Lastly I deleted the unnecesary files in the server directory.

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,3 +1,20 @@
 import {gql} from '@apollo/client';
 
 
+export const GET_ME = gql`
+    query me(userId: ID!) {
+        _id
+        username
+        email
+        password
+        savedBooks {
+            _id
+            authors
+            description
+            bookId
+            image
+            link
+            title
+        }
+    }
+`;
