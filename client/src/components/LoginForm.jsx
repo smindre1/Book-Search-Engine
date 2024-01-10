645ffa425042e884702aefbd78ac20f398e78b7d
Shane Mindreau <smindre1@binghamton.edu>
2024-01-10 12:44:28 -0500

    I adjusted the SignupForm file in the client/src/components directory to utilize graphql.

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -43,7 +43,6 @@ const LoginForm = () => {
       password: '',
     });
   };
-  };
 
   return (
     <>
--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -1,7 +1,8 @@
-import { useState } from 'react';
+import { useState } from 'react'; 
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { createUser } from '../utils/API';
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
@@ -11,7 +12,7 @@ const SignupForm = () => {
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
-
+  const [ addUser, {data, error}] = useMutation(ADD_USER);
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
@@ -28,18 +29,14 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      const { data } = await addUser({
+        variables: { ...userFormData }
+      })
+      // console.log(user);
+      Auth.login(data.addUser.token);
     } catch (err) {
       console.error(err);
-      setShowAlert(true);
+      setShowAlert(true); //Check if this is needed
     }
 
     setUserFormData({
